f6ab23c272dc82fdde487abfeed8bbf673522a1e
Fodé Guirassy <fode.guirassy@ekino.com>
2017-11-21 15:00:05 +0100

    displaying TableView

--- LightControl/LightsTableViewCell.swift
+++ LightControl/LightsTableViewCell.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class LightsTableViewCell: UITableViewCell {
 
+    @IBOutlet var lightPositionLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        //lightPositionLabel?.text = "Sambarou"
         // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- LightControl/LightsViewController.swift
+++ LightControl/LightsViewController.swift
@@ -12,12 +12,45 @@ class LightsViewController: UIViewController {
     
     @IBOutlet weak var lightsTableView: UITableView!
     
+    let lights: [Light] = [Light(position:"Room", isOn:false), Light(position:"Living Room", isOn:false),
+        Light(position:"Hall", isOn:false),
+        Light(position:"Chiken", isOn:false)]
 
-    
-    let lights: [Light] = [Light(position:"Room", isOn:false), Light(position:"Living Room", isOn:false)]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.lightsTableView.delegate = self
+        self.lightsTableView.dataSource = self
+        
+        //self.lightsTableView.register(LightsTableViewCell.self, forCellReuseIdentifier: "cell")
+
+        self.lightsTableView.register(UINib.init(nibName: "LightsTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+        
     }
+}
+
+extension LightsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lights.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? LightsTableViewCell else {
+            fatalError("FUCKEDDDD UP IOS DEVELOPPING")
+        }
+        
+        cell.lightPositionLabel.text = lights[indexPath.row].position
+        return cell
+    
+    }
+
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250
+    }
+    
 }
 
